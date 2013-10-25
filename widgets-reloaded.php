589d314ca673ad77de00fee034af655a8730652a
Justin Tadlock <justin@justintadlock.com>
2013-10-25 01:57:01 -0500

    Version bump to 0.5.1.

--- widgets-reloaded.php
+++ widgets-reloaded.php
@@ -3,7 +3,7 @@
  * Plugin Name: Widgets Reloaded
  * Plugin URI: http://themehybrid.com/plugins/widgets-reloaded
  * Description: Replaces many of the default WordPress widgets with versions that allow much more control.  Widgets come with highly-customizable control panels that provide a ton of flexibility.
- * Version: 0.5.1-alpha
+ * Version: 0.5.1
  * Author: Justin Tadlock
  * Author URI: http://justintadlock.com
  * Text Domain: widgets-reloaded
