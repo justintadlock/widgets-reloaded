e0865e35313f9ee6e09ae8b2471c6c6ede94d0a6
Justin Tadlock <justintadlock@gmail.com>
2017-09-06 14:42:15 -0500

    Leave core widgets enabled by default.  Introduce filter hook for disabling them.

--- widgets-reloaded.php
+++ widgets-reloaded.php
@@ -159,14 +159,17 @@ final class Plugin {
 	public function register_widgets() {
 
 		// Unregister the default WordPress widgets.
-		unregister_widget( 'WP_Widget_Archives'   );
-		unregister_widget( 'WP_Widget_Calendar'   );
-		unregister_widget( 'WP_Widget_Categories' );
-		unregister_widget( 'WP_Widget_Links'      );
-		unregister_widget( 'WP_Nav_Menu_Widget'   );
-		unregister_widget( 'WP_Widget_Pages'      );
-		unregister_widget( 'WP_Widget_Search'     );
-		unregister_widget( 'WP_Widget_Tag_Cloud'  );
+		if ( apply_filters( 'widgets_reloaded_disable_core_widgets', false ) ) {
+
+			unregister_widget( 'WP_Widget_Archives'   );
+			unregister_widget( 'WP_Widget_Calendar'   );
+			unregister_widget( 'WP_Widget_Categories' );
+			unregister_widget( 'WP_Widget_Links'      );
+			unregister_widget( 'WP_Nav_Menu_Widget'   );
+			unregister_widget( 'WP_Widget_Pages'      );
+			unregister_widget( 'WP_Widget_Search'     );
+			unregister_widget( 'WP_Widget_Tag_Cloud'  );
+		}
 
 		// Register custom widgets.
 		register_widget( 'Widgets_Reloaded\Widgets\Archives'   );
@@ -180,7 +183,6 @@ final class Plugin {
 
 		if ( get_option( 'link_manager_enabled' ) )
 			register_widget( 'Widgets_Reloaded\Widgets\Bookmarks' );
-
 	}
 
 	/**
