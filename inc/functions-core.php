f597f7f34779b23f9ffde3cd58d5ae25e356558e
Justin Tadlock <justintadlock@gmail.com>
2017-09-21 12:29:47 -0500

    Add the `WP_Widget_Recent_Posts` widget to the array of potential widgets to disable since it now has an alternative widget.

--- inc/functions-core.php
+++ inc/functions-core.php
@@ -63,13 +63,14 @@ function register_widgets() {
 	// Unregister the default WordPress widgets.
 	if ( ! core_widgets_enabled() ) {
 
-		unregister_widget( 'WP_Widget_Archives'   );
-		unregister_widget( 'WP_Widget_Calendar'   );
-		unregister_widget( 'WP_Widget_Categories' );
-		unregister_widget( 'WP_Widget_Links'      );
-		unregister_widget( 'WP_Nav_Menu_Widget'   );
-		unregister_widget( 'WP_Widget_Pages'      );
-		unregister_widget( 'WP_Widget_Tag_Cloud'  );
+		unregister_widget( 'WP_Widget_Archives'     );
+		unregister_widget( 'WP_Widget_Calendar'     );
+		unregister_widget( 'WP_Widget_Categories'   );
+		unregister_widget( 'WP_Widget_Links'        );
+		unregister_widget( 'WP_Nav_Menu_Widget'     );
+		unregister_widget( 'WP_Widget_Pages'        );
+		unregister_widget( 'WP_Widget_Recent_Posts' );
+		unregister_widget( 'WP_Widget_Tag_Cloud'    );
 	}
 
 	// Register custom widgets.
